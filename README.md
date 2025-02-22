671b3365ee15b9861a4eb0de7bf4585b3ebdac32
difai <threeseas_gz@163.com>
2025-02-23 01:13:50 +0800

    add: Add intranet penetration.

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Node + FFmpeg project for simple music format conversion.
 
 `Note: The system must have FFmpeg installed and it should be added to the environment variables.`
 
+## Globally install dependencies
+
+The npm packages that need to be installed globally are
+
+- localtunnel
+
+```bash
+npm install -g localtunnel
+```
+
 ## Usage Instructions
 
 - Install dependencies
--- routes/index.js
+++ routes/index.js
@@ -11,57 +11,47 @@ const ffmpeg = require('fluent-ffmpeg');
 router.post('/convert-url', async (req, res) => {
   const { url, type } = req.body;
 
-  // deleteAllFilesInDir(path.resolve(__dirname, '../tmp'))
+  deleteAllFilesInDir(path.resolve(__dirname, '../tmp'))
 
-  // if (!url && !type) {
-  //   res.send({ code: 400, data: { url: `` }, msg: 'url and type is required' })
-  //   return
-  // }
+  if (!url && !type) {
+    res.send({ code: 400, data: { url: `` }, msg: 'url and type is required' })
+    return
+  }
 
-  // const match = url.match(/\/(\d+)$/);
-  // let valueFile = ''
+  const match = url.match(/\/(\d+)$/);
+  let valueFile = ''
 
-  // if (match) {
-  //   valueFile = match[1]
-  // } else {
-  //   valueFile = '1000000'
-  // }
-  // const inputFile = url
-  // const outputFile = path.join(__dirname, '../tmp', `${valueFile}.${type}`)
+  if (match) {
+    valueFile = match[1]
+  } else {
+    valueFile = '1000000'
+  }
+  const inputFile = url
+  const outputFile = path.join(__dirname, '../tmp', `${valueFile}.${type}`)
 
-  res.send({
-    code: 200,
-    data: {
-      url: ``,
-      osType: os.type(),
-      osPlatform: os.platform()
-    },
-    msg: 'Conversion Ok'
-  })
-
-  // ffmpeg(inputFile)
-  //   .output(outputFile)
-  //   .audioCodec('libmp3lame')
-  //   .audioBitrate('192k')
-  //   .on('end', () => {
-  //     res.send({
-  //       code: 200,
-  //       data: {
-  //         url: `${config.https}${valueFile}.${type}`
-  //       },
-  //       msg: 'Conversion Ok'
-  //     })
-  //   })
-  //   .on('error', (err) => {
-  //     res.send({
-  //       code: 500,
-  //       data: {
-  //         url: ``
-  //       },
-  //       msg: 'Conversion Error'
-  //     })
-  //   })
-  //   .run();
+  ffmpeg(inputFile)
+    .output(outputFile)
+    .audioCodec('libmp3lame')
+    .audioBitrate('192k')
+    .on('end', () => {
+      res.send({
+        code: 200,
+        data: {
+          url: `${config.https}${valueFile}.${type}`
+        },
+        msg: 'Conversion Ok'
+      })
+    })
+    .on('error', (err) => {
+      res.send({
+        code: 500,
+        data: {
+          url: ``
+        },
+        msg: 'Conversion Error'
+      })
+    })
+    .run();
 })
 
 module.exports = router;
